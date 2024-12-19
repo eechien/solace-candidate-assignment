900b33f54ea43d1bf1b4496752f02c78123a9409
Emily Chien <echien3@gmail.com>
2024-12-19 09:22:45 -0500

    Update advocates route to filter db for searchTerm

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,27 @@
+import { ilike, or, eq, sql } from 'drizzle-orm'
+
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import type { NextRequest } from 'next/server'
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
-
+export async function GET(
+  request: NextRequest,
+) {
+  let searchTerm
+  if (request.nextUrl.searchParams.get('searchTerm')) {
+    searchTerm = request.nextUrl.searchParams.get('searchTerm')
+    searchTerm = `%${searchTerm}%`
+    console.log(searchTerm)
+  }
+  const data = await db.select().from(advocates).where(
+    searchTerm ? (
+      or(
+        ilike(advocates.firstName, searchTerm),
+        ilike(advocates.lastName, searchTerm),
+        ilike(advocates.city, searchTerm),
+        ilike(advocates.degree, searchTerm),
+      )
+    ) : undefined
+  );
   return Response.json({ data });
 }
