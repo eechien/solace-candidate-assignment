06169bd93d487366565b5c8fcdd9fc23872c3354
Emily Chien <echien3@gmail.com>
2024-12-19 09:37:18 -0500

    Move table to Advocates component and break row into Advocate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,17 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
-import Header from './components/Header'
-import SearchForm from './components/SearchForm'
+import Advocates from "./components/Advocates"
+import Header from "./components/Header"
+import SearchForm from "./components/SearchForm"
 import AdvocateContextProvider from './store/advocates-context.jsx';
 
-
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
-
-  useEffect(() => {
-    console.log("fetching advocates...");
-    fetch("/api/advocates").then((response) => {
-      response.json().then((jsonResponse) => {
-        setAdvocates(jsonResponse.data);
-        setFilteredAdvocates(jsonResponse.data);
-      });
-    });
-  }, []);
-
   return (
     <AdvocateContextProvider>
       <main>
         <Header />
         <SearchForm />
-        <table>
-          <thead>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </thead>
-          <tbody>
-            {filteredAdvocates.map((advocate) => {
-              return (
-                <tr>
-                  <td>{advocate.firstName}</td>
-                  <td>{advocate.lastName}</td>
-                  <td>{advocate.city}</td>
-                  <td>{advocate.degree}</td>
-                  <td>
-                    {advocate.specialties.map((s) => (
-                      <div>{s}</div>
-                    ))}
-                  </td>
-                  <td>{advocate.yearsOfExperience}</td>
-                  <td>{advocate.phoneNumber}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <Advocates />
       </main>
     </AdvocateContextProvider>
   );
